6a440e9c3fafd222bf290540b5cce2082945e22c
SWATHI MAKKENA <swathimakkena@pivotal-guest-5-77.ny.pivotallabs.com>
2016-02-02 18:53:25 -0500

    Making some progress

--- spec/clojurehelper/utilities/parser_spec.clj
+++ spec/clojurehelper/utilities/parser_spec.clj
@@ -6,36 +6,140 @@
 
 (def file-path (str (. (File. ".") getCanonicalPath) "spec/clojurehelper/utilities/testclojurefile"))
 
-(def fun-str-one "(defn function1 [param1 param2])")
-(def fun-str-two "(defn function1 [param1 param2] \n(* param1 param2))")
+(def file-str
+  "(ns clojurehelper.core)
 
-(describe 
- "find-all-functions"
+  (defn some-fn [param1 param2]
+    (+ param1 param2))
 
- (before (println (str "canonical path: " file-path)))
- 
- (it "returns a map of the functions in a file with"
-     (should (=
-              (find-all-functions file-path)
-              {:function1 "(defn function1 [param1 param2])"
-               :function2 "(defn function2 [param1 param2 param3])"
-               :function3 "(defn function3 [])"
-               :function4 "(defn function4 [param1])"
-               :function5 "(defn function5 [param1 param2])"}))))
+  (defn another-fn [arg1 arg2]
+    (some-fn arg1 arg2))"
+  )
+
+(defn be-the-same [actual expected]
+  (let [the-same (= actual expected)
+        ]
+    (do
+      (if (not the-same)
+        (println (str "Expected: [" expected "] but got: [" actual "]")))
+      the-same)))
 
 (describe
  "parse"
 
- (it "parses a function from a string"
-     (should (=
-              (parse fun-str-one)
-              {:functions {:function1
-                           {:args ["param1" "param2"]
-                            :body ""}}})))
+ (it "parses a files functions"
+     (should (be-the-same
+              (((parse file-str) :namespace) :functions)
+                {
+                 :clojurehelper.core.new-file-fn {
+                               :params [:clojurehelper.core.param1
+                                        :clojurehelper.core.param2]
+                               :body [:+
+                                      :clojurehelper.core.param1
+                                      :clojurehelper.core.param2]
+                               }
+                 }
+                )))
 
- (it "parses a function with a body from a string"
-     (should (=
-              (parse fun-str-two)
-              {:functions {:function2
-                           {:args ["param1" "param2"]
-                            :body "(* param1 param2)"}}}))))
+ (it "parses a files namespace"
+     (should (be-the-same
+              (((parse file-str) :namespace) :name)
+              "clojurehelper.core.new-file-fn")))
+ )
+
+(describe
+ "seperate-file"
+
+ (it "seperates each of the s-expressions"
+     (should (be-the-same
+              (seperate-file file-str)
+              [
+               "(ns clojurehelper.core)"
+               "(defn some-fn [param1 param2] (+ param1 param2))"
+               "(defn another-fn [arg1 arg2] (some-fn arg1 arg2))"
+               ]))))
+
+(describe
+ "update-current-expression"
+
+ (it "appends the new character to the end of the current expression" 
+     (should (be-the-same
+              (update-current-expression "hi" (first "a"))
+              "hia")))
+
+ (it "does not append the new character to the end of the current expression if it is a newline character"
+     (should (be-the-same
+              (update-current-expression "hi" (first "\n"))
+              "hi")))
+
+ (it "does not append whitespace to the front of a string"
+     (should (be-the-same
+              (update-current-expression "" (first " "))
+              "")))
+
+ (it "does not append whitespace to a string ending in whitespace"
+     (should (be-the-same
+              (update-current-expression "hi " (first " "))
+              "hi "))))
+
+(describe
+ "update-stack"
+
+ (it "adds to the stack when the character is an opening character"
+     (should (be-the-same
+              (update-stack [] (first "("))
+              [(first "(")])))
+
+ (it "adds to the end of the stack when the character is an opening character"
+     (should (be-the-same
+              (update-stack [(first "{")] (first "("))
+              [(first "{")
+               (first "(")])))
+
+ (it "removes from the stack when the character is a closing character"
+     (should (be-the-same
+              (update-stack [(first "{") (first "(")] (first ")"))
+              [(first "{")])))
+
+  (it "does not remove when the character is a closing character but not a match"
+     (should (be-the-same
+              (update-stack [(first "{") (first "(")] (first "]"))
+              [(first "{")
+               (first "(")])))
+
+  (it "does not pop if the stack is empty and the character is nil"
+      (should (be-the-same
+               (update-stack [] nil)
+               [])))
+ )
+
+(describe
+ "update-current-expression"
+
+ (it "adds the expression to the result list when exiting s-expression"
+     (should (be-the-same
+              (update-expressions [] "kewl" [] [""])
+              ["kewl"])))
+ 
+ (it "does not add the expression to the result list when inside s-expression"
+     (should (be-the-same
+              (update-expressions [] "kewl" [""] [""])
+              [])))
+ 
+ (it "does not add the expression to the result list when outside an s-expression"
+     (should (be-the-same
+              (update-expressions [] "kewl" [] [])
+              []))))
+
+(describe
+ "maybe-reset"
+ 
+ (it "resets the expression when the expression has already been added next-expressions"
+     (should (be-the-same
+              (maybe-reset "something something" [1] [1 2])
+              "")))
+ 
+ (it "does not reset the expression when the expression has not been added next-expressions"
+     (should (be-the-same
+              (maybe-reset "something something" [1] [1])
+              "something something"))))
--- src/clojurehelper/utilities/parser.clj
+++ src/clojurehelper/utilities/parser.clj
@@ -1,14 +1,79 @@
 (ns clojurehelper.utilities.parser)
 
-(defn parse [clojure-file])
+(defn get-first-chars [lst]
+  (map
+   (fn [str] (first str))
+   lst))
 
-(defn find-all-functions [file-path]
-  (let [file-string (slurp file-path)]
-    (parse file-string)))
+(def closing
+  (zipmap
+   (get-first-chars [")" "]" "}"])
+   (get-first-chars ["(" "[" "{"])))
 
+(def opening
+  (set (vals closing)))
 
-;(defn function1 [param1 param2])
+(defn update-stack [stack c]
+  (if (contains? opening c)
+    (conj stack c)
+    (let [close-char (closing c)
+          stack-top (last stack)]
+      (if (and
+           (not (= close-char nil))
+           (= close-char stack-top))
+        (pop stack)
+        stack))))
 
-;(defn function2 [param1 param2 param3])
-;(defn function3 [])
-;(defn function4 [param1])(defn function5 [param1 param2])
+(defn- should-retain-character [value character]
+  (and
+   (not (= character (first "\n")))
+   (not
+    (and
+     (= value "")
+     (= character (first " "))))
+   (not
+    (and
+     (= (last value) (first " "))
+     (= character (first " "))))))
+
+(defn update-current-expression [value character]
+  (if (should-retain-character value character)
+    (str value character)
+    value))
+
+(defn update-expressions [expressions expression current-stack last-stack]
+  (let [exiting-expression (and (= 0 (count current-stack)) (= 1 (count last-stack)))]
+    (if exiting-expression
+      (conj expressions expression)
+      expressions)))
+
+(defn maybe-reset [expression expressions next-expressions]
+  (if (> (count next-expressions) (count expressions))
+    ""
+    expression))
+
+(defn seperate-file [clojure-file-as-string]
+  (loop [str clojure-file-as-string
+         current-expression ""
+         stack []
+         expressions []]
+    (if (not (empty? str))
+      (let [c (first str)
+            updated-stack (update-stack stack c)
+            updated-current-expression (update-current-expression current-expression c)
+            updated-expressions (update-expressions expressions updated-current-expression updated-stack stack)
+            maybe-reset-expression (maybe-reset updated-current-expression expressions updated-expressions)]
+          (recur (rest str) maybe-reset-expression updated-stack updated-expressions))
+      expressions)))
+
+(defn parse [clojure-file]
+  (let [seperated-file (seperate-file clojure-file)
+        ]
+    {
+     :namespace
+     {
+      :functions {
+                  }
+      }
+     }))
+
